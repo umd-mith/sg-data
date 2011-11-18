dfc13d661d9c917217b228309518178f6c2bc474
Travis Brown <travisrobertbrown@gmail.com>
2011-11-18 16:33:56 -0500

    Side-stepping Roma for ODD transformations.

--- validation/src/test/scala/edu/umd/mith/sga/xml/ValidatingTest.scala
+++ validation/src/test/scala/edu/umd/mith/sga/xml/ValidatingTest.scala
@@ -3,7 +3,7 @@ package edu.umd.mith.sga.xml
 import edu.umd.mith.util.xml.ValidatingTestBase
 
 class ValidatingTest(name: String) extends ValidatingTestBase(name) {
-  val schema = "/derivatives/shelley_godwin_odd.rnc"
+  val schema = "/schema/rng/shelley-godwin.rng"
   val docs = Seq("/tei/SGA_Esdaile Notebook.xml")
 }
 
